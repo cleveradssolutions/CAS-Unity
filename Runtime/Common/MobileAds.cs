46e02b60587837a63ae2aa5f86fcd9844cfbbe34
Str4tos <eschenkoden@gmail.com>
2023-05-19 10:12:10 +0300

    Beta 3.2.0-rc1

--- Runtime/Common/MobileAds.cs
+++ Runtime/Common/MobileAds.cs
@@ -14,7 +14,7 @@ namespace CAS
         /// <summary>
         /// CAS Unity wrapper version
         /// </summary>
-        public const string wrapperVersion = "3.1.6";
+        public const string wrapperVersion = "3.2.0-rc1";
 
         /// <summary>
         /// Get singleton instance for configure all mediation managers.
