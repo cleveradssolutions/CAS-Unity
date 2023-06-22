6a3302097af33b04a9f37117ec1293793417adc3
Str4tos <eschenkoden@gmail.com>
2023-06-22 14:36:41 +0300

    Release 3.1.9

--- Runtime/Common/MobileAds.cs
+++ Runtime/Common/MobileAds.cs
@@ -14,7 +14,7 @@ namespace CAS
         /// <summary>
         /// CAS Unity wrapper version
         /// </summary>
-        public const string wrapperVersion = "3.1.8";
+        public const string wrapperVersion = "3.1.9";
 
         /// <summary>
         /// Get singleton instance for configure all mediation managers.
