febdc609ca70ec39b9d115022acaed71ff114f8b
d.eshenko <d.eshenko@psvgamestudio.com>
2025-08-27 11:37:36 +0300

    Release 4.5.0-beta1 (Android only)

--- Runtime/Common/MobileAds.cs
+++ Runtime/Common/MobileAds.cs
@@ -11,7 +11,7 @@ namespace CAS
         /// <summary>
         /// The CAS Unity plugin wrapper version.
         /// </summary>
-        public const string wrapperVersion = "4.4.0-rc1";
+        public const string wrapperVersion = "4.5.0-beta1";
 
         /// <summary>
         /// Get singleton instance for configure all mediation managers.
